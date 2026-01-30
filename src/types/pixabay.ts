8ae7be4dccfd89a57ff66b1687547a401b996610
nazar <nazar.kovalchuk17@gmail.com>
2026-01-30 11:57:05 +0200

    Add TypeScript types and refactor to improve type safety

--- src/types/pixabay.ts
+++ src/types/pixabay.ts
@@ -1,3 +1,31 @@
-export type PixabayImage = {};
+export interface PixabayImage {
+  id: number;
+  pageURL: string;
+  type: string;
+  tags: string;
+  previewURL: string;
+  previewWidth: number;
+  previewHeight: number;
+  webformatURL: string;
+  webformatWidth: number;
+  webformatHeight: number;
+  largeImageURL: string;
+  fullHDURL?: string;
+  imageURL?: string;
+  imageWidth: number;
+  imageHeight: number;
+  imageSize: number;
+  views: number;
+  downloads: number;
+  likes: number;
+  comments: number;
+  user_id: number;
+  user: string;
+  userImageURL: string;
+}
 
-export type PixabayResponse = {};
+export interface PixabayResponse {
+  total: number;
+  totalHits: number;
+  hits: PixabayImage[];
+}
